0d378bc8f3429df90fdc09f39f802a3efe743d99
Adam <adamjbrowner@gmail.com>
2016-09-26 19:46:04 +0100

    Basic aesthetic" "

--- js/interface.js
+++ js/interface.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
     var calculation = new Calculation(startingFigure, year);
     calculation.calculateMultiplier()
     calculation.calculateFinal()
-    $('#final-figure').text("£" + calculation.todaysMoney);
+    $('#final-figure').text("£" + calculation.todaysMoney.toFixed(2));
   })
 })
+
+var min = 1900,
+max = min + 50,
+select = document.getElementById('year');
+
+for (var i = min; i<=max; i++){
+   var opt = document.createElement('option');
+   opt.innerHTML = i;
+   select.appendChild(opt);
+}
