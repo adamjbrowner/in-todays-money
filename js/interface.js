717e18e9c3e6f52a0d5b386f6321e62df60623ba
Adam <adamjbrowner@gmail.com>
2016-09-28 14:59:52 +0100

    Coins now appear after calculation

--- js/interface.js
+++ js/interface.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
     var startingFigure = $("#starting-figure").val();
     var year = $("#year").val();
     var calculation = new Calculation(startingFigure, year);
-    calculation.calculateMultiplier()
-    calculation.calculateFinal()
+    calculation.calculateMultiplier();
+    calculation.calculateFinal();
     $('#final-figure').text("£" + calculation.todaysMoney.toFixed(2));
-  })
-})
+
+    var numberOfCoins = calculation.todaysMoney / 100
+    for (i = 1; i < numberOfCoins; i ++) {
+      $("#coins").prepend('<img class="coin" src="images/coin.png" />');
+    };
+  });
+});
